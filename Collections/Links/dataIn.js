972797fe4ff81b0280225bc8294ba422e9881424
Jeremie Miller <jeremie@jabber.org>
2011-08-22 17:40:12 -0500

    making progress towards reworking the incoming data processing

--- Collections/Links/dataIn.js
+++ Collections/Links/dataIn.js
@@ -10,12 +10,51 @@ twitter link bundle?
 
 */
 
-function getEncounterFB(post)
-{
-    
+exports.reIndex = function(locker) {
+    locker.providers(['link/facebook', 'status/twitter'], function(err, services) {
+        if (!services) return;
+        services.forEach(function(svc) {
+            if(svc.provides.indexOf('link/facebook') >= 0) {
+                exports.getLinks("facebook", "newsfeed", svc.id, function() {
+                    exports.getLinks("facebook", "wall", svc.id, function() {
+                        console.error('facebook done!');
+                    });
+                });
+            } else if(svc.provides.indexOf('status/twitter') >= 0) {
+                exports.getLinks("twitter", "home_timeline", svc.id, function() {
+                    console.error('twitter done!');
+                });
+            }
+        });
+    });
 }
 
-function getEncounterTwitter(tweet, cb)
+
+exports.getLinks = function(type, endpoint, svcID, callback) {
+    request.get({uri:lconfig.lockerBase + '/Me/' + svcID + '/getCurrent/' + endpoint}, function(err, resp, body) {
+        var arr;
+        try{
+            arr = JSON.parse(body);            
+        }catch(E){
+            return callback();
+        }
+        processData(svcID, type, endpoint, arr, callback)
+    });
+}
+
+function getEncounterFB(post)
+{
+    var e = {id:post.id
+        , network:"facebook"
+        , text: post.message
+        , from: post.from.name
+        , fromID: post.from.id
+        , at: post.created_time
+        };
+    return e;
+}
+
+function getEncounterTwitter(tweet)
 {
     var e = {id:tweet.id
         , network:"twitter"
@@ -23,5 +62,6 @@ function getEncounterTwitter(tweet, cb)
         , from: tweet.user.name
         , fromID: tweet.user.id
         , at: new Date(tweet.created_at).getTime()
-        }
+        };
+    return e;
 }
---
+++ Collections/Links/testutil.js
@@ -0,0 +1,8 @@
+var util = require('./util.js');
+
+util.expandLink({url:"http://google.com/"},console.log,console.log);
+util.extractFavicon({url:"http://google.com/foo",},console.log,console.log);
+util.extractLinks({text:"hey http://google.com/ can you find bar.com or (bit.ly/foo)?"},console.log,console.log);
+util.fetchHTML({url:"https://github.com/LockerProject/Locker/wiki/As-a-locker-owner%2C-i-want-to-browse-the-links-in-my-locker"},function(html){
+    util.extractText({html:html},console.log,console.log);
+},console.log);
