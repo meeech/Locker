92476cada2028078acc77b4efa768419dc87816d
Jeremie Miller <jeremie@jabber.org>
2011-08-22 11:54:42 -0500

    more fleshing out of the reworked structure

--- Collections/Links/dataIn.js
+++ Collections/Links/dataIn.js
@@ -9,3 +9,19 @@ is fb link info bundled?
 twitter link bundle?
 
 */
+
+function getEncounterFB(post)
+{
+    
+}
+
+function getEncounterTwitter(tweet, cb)
+{
+    var e = {id:tweet.id
+        , network:"twitter"
+        , text: tweet.text
+        , from: tweet.user.name
+        , fromID: tweet.user.id
+        , at: new Date(tweet.created_at).getTime()
+        }
+}
--- Collections/Links/util.js
+++ Collections/Links/util.js
@@ -1,3 +1,22 @@
+// expand an URL
+var unshortener = require('unshortener');
+
+     // you can pass in a url object or string
+     unshortener.expand('http://t.co/rWP6BP3',
+                        function (url) {
+                             // url is a url object
+                             console.log(url);
+                        });
+
+
+exports.expandLink = function(arg, cbEach, cbDone) {
+    if(!arg.url) return cbDone("no url");
+    unshortener.expand(arg.url, function(url){
+        cbEach(url);
+        cbDone();
+    });
+}
+
 /*
 
 extractLinks - text
