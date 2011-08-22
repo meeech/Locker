121998259c48ee7f244b339b4afc5c811bbd270f
Jeremie Miller <jeremie@jabber.org>
2011-08-22 17:01:09 -0500

    utils of fetching, extracting, links, favicon, unshortening seem to be working pretty happily

--- Collections/Links/util.js
+++ Collections/Links/util.js
@@ -1,14 +1,11 @@
-// expand an URL
+
+var url = require('url');
 var unshortener = require('unshortener');
+var sax =  require("sax");
+var readability = require("readabilitySAX");
+var request = require('request');
 
-     // you can pass in a url object or string
-     unshortener.expand('http://t.co/rWP6BP3',
-                        function (url) {
-                             // url is a url object
-                             console.log(url);
-                        });
-
-
+// simply expand a given url
 exports.expandLink = function(arg, cbEach, cbDone) {
     if(!arg.url) return cbDone("no url");
     unshortener.expand(arg.url, function(url){
@@ -17,15 +14,76 @@ exports.expandLink = function(arg, cbEach, cbDone) {
     });
 }
 
-/*
+// find/iterate through all possible urls in any random text
+exports.extractLinks = function(arg, cbEach, cbDone) {
+    if(!arg.text) return cbDone("no text");
+    var regexToken = /((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g;
 
-extractLinks - text
-    normalizes and returns array
-    
-extractText - url
-    returns title and text
+    // when you use /g on a regex it magically maintains state between .exec() calls, CRAZY TIMES!
+    while( (matchArray = regexToken.exec( arg.text )) !== null )
+    {
+        var str = matchArray[0];
+        // gotta do sanity cleanup for url.parse, it makes no assumptions I guess :/
+        if(str.substr(0,4).toLowerCase() != "http") str = "http://"+str;
+        var u = url.parse(str);
+        if(!u) continue;
+        cbEach(u);
+    }
+    cbDone();
+}
 
-expandLink - url
-    returns expanded
+// take the html of a page and extract the title and text
+exports.extractText = function(arg, cbEach, cbDone) {
+    if(!arg.html) return cbDone("no html");
 
-*/
+	var contentLength = 0, skipLevel = 0, 
+		parser, readable, ret;
+
+	// keep skipping unless/until we have a decent amount of text
+	while(contentLength < 250 && skipLevel < 4){
+	    parser = sax.parser(false, {
+		    lowercasetags : true
+		});
+	    
+	    readable = new readability.process(parser, {skipLevel:skipLevel});
+	    
+	    parser.write(arg.html).close();
+	    
+	    ret = readable.getArticle("text");
+	    contentLength = ret.textLength;
+	    skipLevel += 1;
+	}
+	
+	if(ret)
+	{
+	    // normalize all whitespace nicely
+	    ret.text = ret.text.replace(/\s+/g, ' ');
+    	cbEach(ret);
+	}
+	cbDone();
+}
+
+// take the html of a page and determine the favicon
+exports.extractFavicon = function(arg, cbEach, cbDone) {
+    if(!arg.url) return cbDone("no url");
+    // TODO: use regex to look for it in arg.html:
+    // /<link.*?href=("|\')(.*?)("|\').*?rel=("|\').*icon("|\')/i
+    // /<link.*?rel=("|\').*icon("|\').*?href=("|\')(.*?)("|\')/i
+    var u = url.parse(arg.url);
+    cbEach(url.resolve(u,"/favicon.ico"));
+    cbDone();
+}
+
+// handy wrapper
+exports.fetchHTML = function(arg, cbEach, cbDone) {
+    if(!arg.url) return cbDone("no url");
+    // TODO: should accept-only html here, and check response validity
+    request.get({uri:arg.url},function(err,resp,body){
+	   if(err) return cbDone(err);
+	    cbEach(body);
+	    cbDone();
+	});
+	
+}
+
+
