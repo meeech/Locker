8a9b83e228c44e67a20336901a4a769031ad4e06
Thomas Muldowney <temas@sparecog.com>
2011-08-23 13:12:44 -0500

    Fleshed out the calls to store data

--- Collections/Links/dataStore.js
+++ Collections/Links/dataStore.js
@@ -62,18 +62,20 @@ function findWrap(a,b,c,cbEach,cbDone){
 // insert new (fully normalized) link, ignore or replace if it already exists? 
 // {link:"http://foo.com/bar", title:"Foo", text:"Foo bar is delicious.", favicon:"http://foo.com/favicon.ico"}
 exports.addLink = function(link, callback) {
-    linkCollection.findAndModify(XXX, [['_id','asc']], 
-                             {$set:{'url':url}}, 
-                             {safe:true, upsert:true, new: true}, callback);
+    linkCollection.findAndModify({"link":link.link}, [['_id','asc']], {$set:link}, {safe:true, upsert:true, new: true}, callback);
 }
 
 // insert new encounter, replace any existing
 // {id:"123456632451234", network:"foo", at:"123412341234", from:"Me", fromID:"1234", orig:"http://bit.ly/foo", link:"http://foo.com/bar", via:{...}}
 exports.addEncounter = function(encounter, callback) {
     // create unique id as encounter.network+':'+encounter.id+':'+link, sha1 these or something?
-    encounterCollection.findAndModify(XXX, [['_id','asc']], 
-                             {$set:{'url':url}}, 
-                             {safe:true, upsert:true, new: true}, callback);
+    var _hash = encounter.network + ":" + encounter.id + ":" + encounter.link;
+    encounter["_hash"] = _hash;
+    var options = {safe:true, upsert:true, new: true};
+    encounterCollection.findAndModify({"_hash":_hash}, [['_id','asc']], {$set:encounter}, options, function(err, doc) {
+        delete encounter["_hash"];
+        callback(err, doc);
+    });
 }
 
     
